3339b9890df3c25d4faf8ff9b0eba65f811c6bd9
Pier-Luc Richard <84756667+pirichar@users.noreply.github.com>
2025-06-06 19:17:44 -0400

    changed the DB so we now have images

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 - [x] Make it Deploy (vercel)
 - [x] Scaffold basic ui with mock data - https://uploadthing.com/
-- [] Tidy up build process
-- [] ACtually set up a database (vercel postgres)
-- [] Attach dabase to UI
+- [x] Tidy up build process
+- [x] ACtually set up a database (vercel postgres)
+- [x] Attach dabase to UI
 - [] Add athentification (w/ clerk)
 - [] Add image upload
 - [] Error management (w/ sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,22 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"; // This is required to ensure the page is not cached
 
-const mockUrls = [
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhWAvuwvnHoOh7b6lxKXJYrzwBfjNcvLu0s3Zy",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhLAZS4TxOhx97yIMVnYZ8p60ocEvL5i3ktA4J",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhUIcNh0YfRBu2HFg13cfaqwlY8SLthVyexZ5X",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhlGNOuQLzKEI6xDjkZ5NXcHouA1TShq8VLapY",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhRkjbn46jyLSOF2s3Ad4plcx5abgtQ0MwNPkV",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhVJq9z8l3tjk4sH0FVnZhc15BdJ2Cf6eXYEiz",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7HhZlKOlwmz7Hh2B6OQitPlL4gfFYybqk18w5KN",
-  "https://mtt6p0kz3e.ufs.sh/f/ZU5aSpcmz7Hh8NO1WWVqhI4w8YjzSWnOkVTi1BmRltcQ3MF5",
- ]
-
- const mockImages = mockUrls.map((url, index) => ({
-    id : index + 1,
-    url,
- }));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   
-  console.log(posts);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>)
-          )}
-        {[...mockImages,...mockImages,...mockImages].map((image, index) =>(
-          <div key={image.id + "-" + index} className="w-48">
+
+        {[...images,...images,...images].map((image, index) =>(
+          <div key={image.id + "-" + index} className="w-48 flex-col">
             <img src={image.url} />
+            <div>
+              <p className="text-sm">{image.name}</p>
+              </div>
           </div>
         ))}
       </div>
